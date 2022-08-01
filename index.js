3b85d31ee91fe3404b4dda3f8a6a581e173fba71
Taufiq Gilang <adhitama1310@gmail.com>
2022-08-02 04:43:00 +0700

    Pemisahan source js

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-import {ethers} from "./ethers-5.6.esm.min.js"
+import {Contract, ethers} from "./ethers-5.6.esm.min.js"
 import {abi, contractAddress} from "./constants.js"
 
 const connectButton = document.getElementById("connectButton")
-const transferButton = document.getElementById("transferButton")
-const payoffButton = document.getElementById("payoffButton")
 const isiformButton = document.getElementById("isiformButton")
 connectButton.onclick = connect
-transferButton.onclick = transfer
-payoffButton.onclick = payoff
-isiformButton.onclick = isiform
+isiformButton.onsubmit = isiform
 
 console.log(ethers)
 console.log(abi)
@@ -27,24 +23,24 @@ async function connect()
     }
 }
 
-async function transfer()
+async function isiform()
 {
-    const ethAmount = document.getElementById("ethAmount").value
-    const destination = document.getElementById("destination").value
-    console.log("Fund with {ethAmount}")
+    const lender = document.getElementById("lender").value
+    const borrower = document.getElementById("borrower").value
+    const loanAmount = parseInt(document.getElementById("loanAmount").value)
+    const payoffAmount = parseInt(document.getElementById("payoffAmount").value)
+    const loanDuration = parseInt(document.getElementById("loanDuration").value)
+
     if (typeof window.ethereum !== "undefined")
     {
         try
         {
             const provider = new ethers.providers.Web3Provider(window.ethereum)
             const signer = provider.getSigner()
-            ethers.utils.getAddress(destination);
-            const tx = await signer.sendTransaction({
-            to: destination,
-            value: ethers.utils.parseEther(ethAmount)
-          });
-          console.log({ ethAmount, destination });
-          console.log("tx", tx);
+            const contract = new ethers.Contract(contractAddress, abi, signer)
+            const tx = await contract.IsiForm(lender, borrower, loanAmount, payoffAmount, loanDuration);
+            await tx.wait();
+            console.log(tx, tx);
         }
         catch(error)
         {
@@ -52,51 +48,6 @@ async function transfer()
         }
     }
 }
-
-async function isiform()
-{
-    const lender = document.getElementById("lender").value
-    const borrower = document.getElementById("borrower").value
-    const loanAmount = document.getElementById("loanAmount").value
-    const payoffAmount = document.getElementById("payoffAmount").value
-    const loanDuration = document.getElementById("loanDuration").value
-    {
-        try
-        {
-          console.log({ lender, borrower, loanAmount, payoffAmount,loanDuration });
-        }
-        catch(error)
-        {
-            console.log(error)
-        }
-    }
-}
-
-// async function payoff()
-// {
-//     const payoffAmount = document.getElementById("payoffAmount").value
-//     const lender = document.getElementById("lender").value
-//     console.log("Fund with {ethAmount}")
-//     if (typeof window.ethereum !== "undefined")
-//     {
-//         try
-//         {
-//             const provider = new ethers.providers.Web3Provider(window.ethereum)
-//             const signer = provider.getSigner()
-//             ethers.utils.getAddress(lender);
-//             const tx = await signer.sendTransaction({
-//             to: lender,
-//             value: ethers.utils.parseEther(payoffAmount)
-//           });
-//           console.log({ payoffAmount, lender });
-//           console.log("tx", tx);
-//         }
-//         catch(error)
-//         {
-//             console.log(error)
-//         }
-//     }
-// }
 
 
 function WaitingResponse(transactionResponse, provider)
@@ -111,9 +62,3 @@ function WaitingResponse(transactionResponse, provider)
         resolve()
     })
 }
-
-
-
-// async function withdraw(){
-//     if(typeof window.ethereum)
-// }
