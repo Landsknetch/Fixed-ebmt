68469cb17889705f8674760dfb7e7c6c9fc4e17f
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2022-08-06 01:06:10 +0700

    update

--- mongoose.js
+++ mongoose.js
@@ -1,5 +1,5 @@
-// var mongoose = require('mongoose');
-import { mongoose } from './node_modules/mongoose/index.js'
+const mongoose = require('mongoose');
+// import { mongoose } from './node_modules/mongoose/index.js'
 await mongoose.connect('mongodb+srv://DragoonCrew:9011@cluster0.q7bte.mongodb.net/?retryWrites=true&w=majority');
 const Loan = mongoose.model('Loan', {
     lender_address: String,
